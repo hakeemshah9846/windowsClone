8446676fc059fff35e72f1a2172ba55ca2b781cb
SHAH-1846 <hakeem.shah@haatch.in>
2025-02-22 16:01:57 +0530

    Added Interactive Torch Effect – Smooth Mouse Tracking & Hidden Switch Challenge!

--- src/components/apps/Torch.jsx
+++ src/components/apps/Torch.jsx
@@ -17,10 +17,77 @@ export default function Torch(input, setInput) {
         y : useSpring(mouse.y, smoothOptions),
     };
 
+    const [showTorch, setShowTorch] = useState(true);
+    const [showIntermediate, setShowIntermediate] = useState(true);
+    const [randomPosition, setRandomPosition] = useState({
+        top : "50%",
+        left : "50%",
+    });
+
+    const audio = useMemo(() => new Audio("/audio.switch.mp3"),[]);
+
+
+    useEffect(() => {
+        audio.load();
+
+        const manageMouseMove = (e) => {
+            const {clientX, clientY} = e;
+            mouse.x.set(clientX - cursorSize / 2);
+            mouse.y.set(clientY - cursorSize / 2);
+        };
+
+        window.addEventListener("mousemove", manageMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", manageMouseMove);
+        };
+    }, [mouse.x, mouse.y, cursorSize, audio])
+
+
+    const playSound = () => {
+        audio.play().catch((error) => {
+            console.error("Failed to play audio : ", error);
+        })
+    };
+
 
     return(
         <main className="cursor-custom w-full h-full">
-            Torch Component
+            {showIntermediate && (
+                <div className="w-full h-screen bg-white absolute top-0 z-50 flex justify-center items-center text-black font-bold text-5xl flex-col">
+                    Why would you close the window :C
+                    <span className="block">
+                        now find the switch and open the window AGAIN!!!
+                    </span>
+                </div>
+            )}
+
+            { showTorch && (
+                <>
+                <motion.div style={{
+                    zIndex : 1000,
+                    left : smoothMouse.x,
+                    top : smoothMouse.y,
+                    height : cursorSize,
+                    width : cursorSize,
+                    position : "fixed",
+                    pointerEvents : "none",
+                    visibility : "visible",
+                    background : "radial-gradient(circle at 50% 50%, rgba(20, 20, 20, 0) 0%, rgba(20, 20, 20, 0.4) 1.25%, rgba(20, 20, 20, 0.6) 1.5%, rgba(20, 20, 20, 0.9) 1.875%, rgba(20, 20, 20, 1) 2.5%, rgba(20, 20, 20, 1) 100%)",
+                }} className="text-primary"></motion.div>
+
+                <div className="w-full h-screen absolute top-0 bg-transparent z-50">
+                    <img src="images/apps/switch.png" alt="switch" className="h-10 w-10 z-50 absolute hover:cursor-pointer" style={{top : randomPosition.top, left : randomPosition.left}} 
+                    onClick={() => {
+                        playSound();
+                        setInput(null);
+                    }} 
+                    role="button" 
+                    aria-label="Find the switch to reopen the window" 
+                    />
+                </div>
+                </>
+            )}
         </main>
     )
 
